16bfb9c57b28fb6e1fac741150d560de4a12c716
Diego Lopes <dihgg@dihgg.com>
2025-06-02 14:30:26 -0300

    chore(tooling): adding tests and mocks

--- src/client/example/Example.spec.ts
+++ src/client/example/Example.spec.ts
@@ -1,4 +1,3 @@
-// src/client/example/Example.spec.ts
 import * as ExampleAPI from './api/ExampleAPI';
 import * as PipeWrench from '@asledgehammer/pipewrench';
 import * as Events from '@asledgehammer/pipewrench-events';
@@ -10,6 +9,7 @@ jest.mock('@asledgehammer/pipewrench-events');
 describe('Example module', () => {
 	
 	it('registers onGameStart listener and calls addRedSquare and greetPlayer', () => {
+
 		(PipeWrench.getPlayer as jest.Mock).mockReturnValue('player');
 		const addListener = (Events.onGameStart.addListener as jest.Mock);
 		require('./Example');
